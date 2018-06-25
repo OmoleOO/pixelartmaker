70c4c28d56dc35903e757673baa88f1b8cdf848c
Olumide Omole <olumideomole@gmail.com>
2018-06-25 23:32:37 +0100

    Day 2: #7daysofcodechallenge Updated Readme.md;  Replaced JQuery with vanilla JS; Changed popover style

--- README.md
+++ README.md
@@ -3,13 +3,28 @@
 ## Table of Contents
 
 * [Introduction](#introduction)
+* [Instructions](#instructions)
 * [Contributing](#contributing)
 
 ## Introduction
 
-Create a custom grid or use the default grid, and apply colors to those squares to create your digital masterpiece.
+<p><b>Create a custom grid or use the default grid, and apply colors to those squares to create your digital masterpiece.</b></p>
 
-Visit: https//omoleoo.github.io/pixelartmaker/
+<p><i>This is in partial fulfillment of the requirements for the award of the Google Africa Challenge Scholarship for the Udacity Front-end Web Development Nanodegree programme.</i></p>
+
+
+https//omoleoo.github.io/pixelartmaker/
+
+
+## Instructions
+
+* Drag + mouseover grid to paint.
+* Ctrl + mouseover grid to erase paint.
+* Shift + mouseover grid to temporarily suspend painting.
+
+
+https//omoleoo.github.io/pixelartmaker/
+
 
 ## Contributing
 
--- js/designs.js
+++ js/designs.js
@@ -1,111 +1,156 @@
 
-
  
 
-$(function(){
-    let chosenColour = $('#colorPicker').val();
-    let colours = [];
-    let palette = $('.palette');
 
-    // Show grid by default
-    makeGrid();
-    $('#sizePicker').hide();
+let chosenColour = document.getElementById("colorPicker").value;
+let colours = [];
+let palette = document.getElementById("palette");
+let table = document.getElementById('pixelCanvas');
+let sizePicker = document.getElementById('sizePicker');
+let colorPicker = document.getElementById("colorPicker");
+let gridSize = document.getElementById('gridSize');
+let submitBtn = document.getElementById('btn-submit');
+let clearCanvas = document.getElementById("clear-canvas");
+let gridOption = document.querySelector("input[type=radio]");
+let popover = document.querySelector(".popover");
+let helpBtn = document.getElementById("help")
 
-    //Clear canvas
-    document.getElementById("clear-canvas").addEventListener('click', function() {
-        let rows = document.getElementById("pixelCanvas").rows;
-        for (let row of rows){
-            for (let cell of row.cells){
-                cell.style.backgroundColor = '#fff'
-            }
-        } 
-    });
-
-    /**
-     * @description creates default or custom grid
-     * @param {*height} h 
-     * @param {*width} w 
-     */
-    function makeGrid(h, w){
-        let height = h || 10; 
-        let width = w || 40;
-        let table = $('#pixelCanvas');
-        table.children().remove();
-        let x = 1; 
-        while ( x <= height ){
-            let tr = $('<tr></tr>');
-            table.append(tr);
-            for (let j = 1; j <= width; j++) {
-                tr.append('<td></td>');
-            } 
-            x++
-        }  
-    }
-
-    // Grid size option
-    $('#gridSize').on('change', 'input[type=radio]', function(evt){
-        if(evt.target.value === 'Default'){
-            makeGrid();
-            $('#sizePicker').hide();
-        } else {
-            $('#sizePicker').show();
-            $('#pixelCanvas').children().remove();
-        }
-    });
-
-    // Draw custom grid
-    $('#btn-submit').on('click', function(evt){
-        evt.preventDefault();
-        let inputHeight = $('#inputHeight').val(); 
-        let inputWidth = $('#inputWidth').val();
-        let errorMsg = [
-            "Grid height should be less than 25", 
-            "Grid width should be less than 100"
-        ];
-        if ((inputHeight > 25) || (inputWidth > 100)) {
-           alert(`${inputHeight > 25 ? errorMsg[0] : ""} ${inputWidth > 100 ? errorMsg[1] : ""}`); 
-           return;
-        }
-        else {
-            $('main').css('height', 'auto');
-            makeGrid(inputHeight, inputWidth);
-        }
-    });
-
-    // Grid controls
-    $('#pixelCanvas').on('mouseover', 'td', function(evt){
-        // To draw: mouseover grid 
-        $( evt.target ).css('background-color', chosenColour);
-         
-        if (evt.ctrlKey){ 
-            // To erase: Ctrl + mouseover grid
-            $( evt.target ).css('background-color', '#fff');
-        }
-    });
-    
-    // Monitors colorPicker
-    $('#colorPicker').on('change', function(){ 
-        chosenColour = $('#colorPicker').val();
-        // Add new colour to palette
-        if (!colours.includes($(this).val())){ 
-            colours.splice(0, 0, $(this).val());
-        }
-        palette.children().remove();
-
-        colours.forEach(function(colour, index, colours){
-            if (index <= 6){ // Colour palette holds 7 recently used colours
-                let tr = $(`<tr style="background-color: ${colour}"></tr>`);
-                tr.append(`<td style="padding: 10px">${colour}</td>`)
-                palette.append(tr);
-            }
-        });
-
-        // Monitor the palette
-        $('.palette').on('click', 'tr', function(evt){ 
-            $(evt.target).css('cursor', 'pointer');
-            // Select colour from palette
-            chosenColour = $(this).css('background-color'); 
-        });
-    });
-    
+popover.style.visibility = 'hidden';
+helpBtn.addEventListener("mouseover", function(evt){
+    popover.style.visibility = 'visible';
 });
+
+helpBtn.addEventListener("mouseleave", function(evt){
+    popover.style.visibility = 'hidden';
+});
+
+// Show grid by default
+if (gridOption.value == "Default"){
+    makeGrid();
+    gridControl();
+}
+
+
+//Clear canvas
+clearCanvas.addEventListener('click', function() {
+    let cells = document.querySelectorAll('#pixelCanvas td');
+    cells.forEach(cell => cell.style.backgroundColor = '#fff');
+});
+
+// Grid size option
+gridSize.addEventListener("change", function(evt) {
+    if(evt.target.value === 'Default'){
+        sizePicker.style.display = 'none';
+        makeGrid();
+        gridControl();
+    } else {
+        sizePicker.style.display = 'block';
+        clearTableRows("#pixelCanvas tr");
+    }
+});
+
+
+// Draw custom grid
+submitBtn.addEventListener('click', function(evt){
+    evt.preventDefault();
+    let inputHeight = document.getElementById("inputHeight").value; 
+    let inputWidth = document.getElementById("inputWidth").value;
+    let errorMsg = [
+        "Grid height should be less than 25", 
+        "Grid width should be less than 100"
+    ];
+    if ((inputHeight > 25) || (inputWidth > 100)) {
+        alert(`${inputHeight > 25 ? errorMsg[0] : ""} ${inputWidth > 100 ? errorMsg[1] : ""}`); 
+        return;
+    }
+    else {
+        document.getElementsByTagName("main")[0].style.height = 'auto';
+        makeGrid(inputHeight, inputWidth);
+        gridControl();
+    }
+});
+
+
+// Monitors colorPicker
+colorPicker.addEventListener('change', function(){ 
+    chosenColour = colorPicker.value;
+    // Add new colour to palette
+    if (!colours.includes(chosenColour)){ 
+        colours.splice(0, 0, chosenColour);
+    }
+    clearTableRows("#palette tr");
+
+    // Build and fill palette
+    colours.forEach(function(colour, index, colours){
+        // Colour palette holds 7 recently used colours
+        if (index <= 6){ 
+            let tr = document.createElement("tr");
+            let td = document.createElement("td");
+            td.style.padding = '10px';
+            td.innerHTML = colour;
+            td.style.backgroundColor = colour;
+            tr.appendChild(td)
+            palette.appendChild(tr);
+        }
+    });
+
+    // Monitor the palette
+    let paletteCells = document.querySelectorAll('#palette tr');
+    paletteCells.forEach(cell => cell.addEventListener('click', function(evt){ 
+        evt.target.style.cursor = 'pointer';
+        // Select colour from palette
+        chosenColour = evt.target.style.backgroundColor;
+    }));
+});
+
+/**
+ * @description Clear table rows
+ * @param {*String query selector} querySelector
+ */
+function clearTableRows(querySelector){
+    document.querySelectorAll(querySelector)
+        .forEach(row => row.remove());
+}
+
+/**
+ * @description creates default or custom grid
+ * @param {*int height} h 
+ * @param {*int width} w 
+ */
+function makeGrid(h, w){
+    let height = h || 10; 
+    let width = w || 40;
+    clearTableRows("#pixelCanvas tr");
+    let x = 1; 
+    while ( x <= height ){
+        let tr = document.createElement("tr");
+        table.appendChild(tr);
+        for (let j = 1; j <= width; j++) {
+            let td = document.createElement("td");
+            tr.appendChild(td);
+        } 
+        x++
+    }  
+}
+
+/**
+ * @description Controls grid's painting work follow
+ * mouseover grid to paint | Ctrl + mouseover grid to erase |
+ * Shift + mouseover grid to temporarily suspend painting
+ */
+function gridControl(){
+    // Grid controls
+    let gridCells = document.querySelectorAll('#pixelCanvas td');
+    gridCells.forEach(cell => cell.addEventListener('mouseover', function(evt){ 
+        if (evt.shiftKey){ 
+            // To suspend painting: Shift + mouseover grid
+            return false;
+        } else if (evt.ctrlKey){ 
+            // To erase: Ctrl + mouseover grid
+            evt.target.style.backgroundColor = '';
+        } else {
+            // To paint: mouseover grid
+            evt.target.style.backgroundColor = chosenColour;
+        }
+    }));
+}
